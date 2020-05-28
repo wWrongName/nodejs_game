71af36c1921131c1ff80a98573357d37cd97df35
wWrongName <name.johnny@mail.ru>
2020-05-28 17:35:28 +0300

    prepare for merging

--- openApi.js
+++ openApi.js
@@ -1,5 +1,5 @@
-const HSPort = ":" + 1234;
-const WSPort = ":" + 1235;
+const HSPort = 1234;
+const WSPort = 1235;
 //const ip   = "localhost";
 const ip     = "95.217.212.200";
 const path   = "/root/nodejs_game/"; // change project path <<===|\|
@@ -10,7 +10,9 @@ const apiSHeader = {
             title : "API for nodejs-game",
             version : "1.0.0",
             description : "This is an API for simple server game, that is based on websockets",
-            servers : ["http://" + ip + HSPort, "ws://" + ip + WSPort]
+            servers : ["http://" + ip + ":" + HSPort, "ws://" + ip + ":" + WSPort],
+            HSPort : HSPort,
+            WSPort : WSPort
         }
     },
     apis : [path + "/v1/api.js"],
